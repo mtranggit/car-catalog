2aa386a90f5a4470a97ff2354daf2d812174b91e
Michael Trang <michael.trang@hotmail.com>
2018-11-01 00:23:49 +1100

    remove App.test.js

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import {render} from 'react-testing-library'
-import {App} from './App'
-
-it.skip('renders without crashing', () => {
-  const {getByText} = render(<App />)
-  expect(getByText('')).toBeInTheDOM()
-})
